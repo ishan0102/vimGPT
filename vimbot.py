7925d53f37472f1da3f8cef42122ca112beb23dd
Ishan Shah <ishan0102@utexas.edu>
2023-11-07 21:10:30 -0500

    Set up available functions

--- vimbot.py
+++ vimbot.py
@@ -25,13 +25,16 @@ class Vimbot:
         self.page = self.context.new_page()
         self.page.set_viewport_size({"width": 1280, "height": 1080})
 
-    def go_to_page(self, url):
+    def navigate(self, url):
         self.page.goto(url=url if "://" in url else "https://" + url, timeout=60000)
 
     def type(self, text, submit=False):
         self.page.keyboard.type(text)
         if submit:
             self.page.keyboard.press("Enter")
+
+    def click(self, text):
+        self.page.type(text)
 
     def capture(self, path):
         # capture a screenshot with vim bindings on the screen
