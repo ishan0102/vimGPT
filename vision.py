8f4378c66ee2c7b0172ea8ac531ffa7814e4b222
Ben Field <fieldben97@gmail.com>
2023-12-22 13:15:44 +1100

    switched to instrucvtor library for forcing output adheremce

--- vision.py
+++ vision.py
@@ -4,12 +4,20 @@ import os
 from io import BytesIO
 
 import openai
+import instructor
 from dotenv import load_dotenv
 from PIL import Image
+from openai import OpenAI
+from pydantic import BaseModel
+from typing import Optional
+from instructor import Mode
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 IMG_RES = 1080
+
+# Enables `response_model`
+client = instructor.patch(OpenAI(), mode=instructor.function_calls.Mode.MD_JSON)
 
 
 # Function to encode the image
@@ -21,18 +29,50 @@ def encode_and_resize(image):
     encoded_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
     return encoded_image
 
+class action_details(BaseModel):
+    
+    action: str
+    character_string: str
+    confidence: str
+    type_input : Optional[str]
+    url : Optional[str]
+    done : Optional[None]
 
-def get_actions(screenshot, objective):
+
+def get_actions(screenshot, objective, previous_action):
     encoded_screenshot = encode_and_resize(screenshot)
-    response = openai.chat.completions.create(
+    example_json = '''
+        {
+            'action': 'character sequence',
+            'character_string': str
+            'type input': str
+            url: str # optional
+            'done': None #optional
+        }
+    '''
+    response = client.chat.completions.create(
         model="gpt-4-vision-preview",
+        response_model=action_details,
         messages=[
             {
                 "role": "user",
                 "content": [
                     {
                         "type": "text",
-                        "text": f"You need to choose which action to take to help a user do this task: {objective}. Your options are navigate, type, click, and done. Navigate should take you to the specified URL. Type and click take strings where if you want to click on an object, return the string with the yellow character sequence you want to click on, and to type just a string with the message you want to type. For clicks, please only respond with the 1-2 letter sequence in the yellow box, and if there are multiple valid options choose the one you think a user would select. For typing, please return a click to click on the box along with a type with the message to write. When the page seems satisfactory, return done as a key with no value. You must respond in JSON only with no other fluff or bad things will happen. The JSON keys must ONLY be one of navigate, type, or click. Do not return the JSON inside a code block.",
+                        "text": f"""You are a web navigator who needs to choose which action to take to achieve objective: {objective}. Your last action was: {previous_action}. 
+                        Considering your overall objective, the current state of the browser page, select your next step. 
+                        Your action options are navigate, type, click, and done. 
+                        If you select navigatge, you must specify a URL. 
+                        If you select type or string, you must specify the yellow character sequence you want to click on, and to type you must specify the input to type. 
+                        For clicks, please only specify with the 1-2 letter sequence in the yellow box, and if there are multiple valid options choose the one you think a user would select. 
+                        For typing, please specify a click to click on the box along with a type with the message to write. 
+                        When the page seems satisfactory, return done as a key with no value. 
+                        You must specify a level of confidence that your decision will further the objective. Please think closely about this, as there are alternatives if you are not confident. 
+                        The value of the confidence key must be one of LOW, MEDIUM, HIGH scoring of your confidence that your action is correct.
+
+                                The outputted JSON MUST look like:
+                                {example_json}
+                        """,
                     },
                     {
                         "type": "image_url",
@@ -43,29 +83,15 @@ def get_actions(screenshot, objective):
                 ],
             }
         ],
-        max_tokens=100,
+        max_tokens=200,
     )
 
-    try:
-        json_response = json.loads(response.choices[0].message.content)
-    except json.JSONDecodeError:
-        print("Error: Invalid JSON response")
-        cleaned_response = openai.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant to fix an invalid JSON response. You need to fix the invalid JSON response to be valid JSON. You must respond in JSON only with no other fluff or bad things will happen. Do not return the JSON inside a code block."},
-                {"role": "user", "content": f"The invalid JSON response is: {response.choices[0].message.content}"}
-            ]
-        )
-        try: cleaned_json_response = json.loads(cleaned_response.choices[0].message.content)
-        except json.JSONDecodeError:
-            print("Error: Invalid JSON response")
-            return {}
-        return cleaned_json_response
-
-    return json_response
+    print(response)
+    return response
 
 
 if __name__ == "__main__":
     image = Image.open("image.png")
     actions = get_actions(image, "upvote the pinterest post")
+
+
